4968c576db142bead8ed7c097b6e9ab5c9855ef0
TejasVijaya74 <vt6267700@gmail.com>
2026-02-21 23:24:18 +0530

    Add configuration and logging system

---
+++ app/core/config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
---
+++ app/core/exceptions.py
@@ -0,0 +1,2 @@
+class GeminiAPIError(Exception):
+    pass
